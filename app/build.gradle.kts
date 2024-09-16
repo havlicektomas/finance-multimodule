30368ba5af2aa6b117a2f8cf925e6097eeca9419
Tomas Havlicek <havlicek.tomas@gmail.com>
2024-09-16 17:58:38 +0200

    Implement android application compose convention plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.runique.android.application)
+    alias(libs.plugins.runique.android.application.compose)
 }
 
 android {
@@ -10,12 +10,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
@@ -43,7 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
