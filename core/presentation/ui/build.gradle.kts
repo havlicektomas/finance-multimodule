da270c865d157ec938bc3514d02b624a9133a790
Tomas Havlicek <havlicek.tomas@gmail.com>
2024-10-01 18:00:41 +0200

    Implement UiText

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -10,10 +10,10 @@ dependencies {
     implementation(projects.core.domain)
     implementation(projects.core.presentation.designsystem)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 }
