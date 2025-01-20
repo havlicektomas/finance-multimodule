1d8a8774d48eb21152f85b08d7b733b24397e45d
Tomas Havlicek <havlicek.tomas@gmail.com>
2025-01-20 19:09:24 +0100

    Update java version

--- build-logic/convention/src/main/java/dev/havlicektomas/convention/Kotlin.kt
+++ build-logic/convention/src/main/java/dev/havlicektomas/convention/Kotlin.kt
@@ -17,8 +17,8 @@ internal fun Project.configureKotlinAndroid(
         defaultConfig.minSdk = libs.findVersion("projectMinSdkVersion").get().toString().toInt()
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
     }
 
@@ -31,8 +31,8 @@ internal fun Project.configureKotlinAndroid(
 
 internal fun Project.configureKotlinJvm() {
     extensions.configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     configureKotlin()
 }
@@ -40,7 +40,7 @@ internal fun Project.configureKotlinJvm() {
 private fun Project.configureKotlin() {
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_11.toString()
+            jvmTarget = JavaVersion.VERSION_17.toString()
         }
     }
 }
