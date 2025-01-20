6079af9d3ce097a24bd8ce3d2ecd493d92ab7e35
Tomas Havlicek <havlicek.tomas@gmail.com>
2025-01-20 16:12:27 +0100

    Update project to use kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
     alias(libs.plugins.androidDynamicFeature) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
